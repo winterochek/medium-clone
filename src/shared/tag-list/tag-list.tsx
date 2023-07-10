1cdcf03170111188c7323fd2dfd4418e9cf2f2ba
Pavel Zherebnov <pavlozherebnov@gmail.com>
2023-07-10 18:33:07 +0300

    fix:tag-list key prop

--- src/shared/tag-list/tag-list.tsx
+++ src/shared/tag-list/tag-list.tsx
@@ -1,8 +1,11 @@
 export default function TagListComponent({ tags }: { tags: string[] }) {
    return (
       <div className='flex flex-row flex-wrap gap-2'>
-         {tags.map(tag => (
-            <div className='inline-block border border-gray-500 text-gray-500 p-1 rounded-xl overflow-hidden text-sm hover:cursor-pointer select-none'>
+         {tags.map((tag, i) => (
+            <div
+               key={`${tag}/${i}`}
+               className='inline-block border border-gray-500 text-gray-500 p-1 rounded-xl overflow-hidden text-sm hover:cursor-pointer select-none'
+            >
                {tag}
             </div>
          ))}
